d1addf42e911c4dbafc23e6e747b3b24ebc1ab65
pinktulip24 <andreeak_09@yahoo.com>
2025-02-16 21:02:47 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -38,7 +38,7 @@ const Cast = () => {
       {hasError && <Error />}
       {notFound && <h1>Requested movie does not exist</h1>}
       {cast && cast.length > 0 && (
-        <div className="row row-cols-1 row-cols-lg-5 g-4">
+      <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4"> 
           {cast.map(actor => (
             <div key={actor.id}>
               {actor.profile_path ? (
